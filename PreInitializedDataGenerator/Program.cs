c615a96ff778be24e9516eb9771f7616e05b096e
CeniusM <93879193+CeniusM@users.noreply.github.com>
2023-02-09 19:21:01 +0100

    Changes

---
+++ PreInitializedDataGenerator/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+void PrintArray(int[] offsets)
+{
+    StringBuilder output = new StringBuilder(offsets.Length * 10);
+
+    for (int i = 0; i < offsets.Length; i++)
+    {
+        output.Append(offsets[i].ToString() + ',');
+    }
+    output.Remove(output.Length - 1, 1);
+    Console.WriteLine(output.ToString());
+}
+
+
+PrintArray(new int[] { 1, 124, 125, 54 });
+Console.ReadLine();
